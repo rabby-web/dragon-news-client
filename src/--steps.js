0a8069e5e83999576abdb973c05258766c9179c7
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-04 11:48:00 +0600

    react router setup start

--- src/--steps.js
+++ src/--steps.js
@@ -1,4 +1,5 @@
 /*
+    BASIC PROJECT SETUP
 1. create firebase app
 2. get firebase configuration inside firebase.config.js
 3. make sure to export app from firebase config
@@ -6,4 +7,9 @@
 5. install firebase and react router dom
 6. install bootstrap and react-bootstrap
 7. make sure to inport bootstrap css link import main.jsx
+*/ 
+
+/*
+    ROUTER SETUP
+1. 
 */ 
--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './Routes/Routes/Routes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Bangladesh</h1>
+      <RouterProvider router={routes}></RouterProvider>
     </>
   )
 }
