e20d1443c33ea3eb13e43f7af3ac1f67f0ed6fa0
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-07 14:34:37 +0600

    navber added the heardaer

--- src/Pages/Shared/BrandCarousel/BrandCarousel.jsx
+++ src/Pages/Shared/BrandCarousel/BrandCarousel.jsx
@@ -1,4 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
+import Brand1 from '../../../assets/brands/daraz-2.png';
+import Brand2 from '../../../assets/brands/daraz-3.png';
 
 const BrandCarousel = () => {
     return (
@@ -6,25 +8,21 @@ const BrandCarousel = () => {
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
+                src={Brand1}
                 alt="First slide"
               />
               <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
               </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
+                src={Brand2}
                 alt="Second slide"
               />
 
               <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
