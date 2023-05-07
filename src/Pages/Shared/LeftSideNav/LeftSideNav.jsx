3af7cc9c9510e143d6ef7ffde0b9021f63a37fce
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-07 12:13:56 +0600

    API load from dragon news servar

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,10 +1,25 @@
-// import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-    // const [category, setCategory] = useState([]);
+    const [category, setCategory] = useState([]);
+
+    useEffect( () => {
+        fetch('http://localhost:5000/news-categories')
+        .then( res => res.json())
+        .then(data => setCategory(data));
+    }, [])
+
     return (
         <div>
-            <h4>All Category</h4>
+            <h4>All Category: {category.length}</h4>
+            <div>
+                {
+                    category.map(category => <p key={category.id}>
+                        <Link>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
