3e7d3bddb5669c199a3b12b35d7109930d9eff54
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-07 14:52:40 +0600

    nav ediet

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -2,16 +2,17 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import LeftSideNav from '../LeftSideNav/LeftSideNav';
 
 const Header = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar className='mb-4' collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
               <Navbar.Brand href="#home">Dragon News</Navbar.Brand>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="#features">Features</Nav.Link>
+                  <Nav.Link href="#features">All News</Nav.Link>
                   <Nav.Link href="#pricing">Pricing</Nav.Link>
                   <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -32,6 +33,9 @@ const Header = () => {
                   </Nav.Link>
                 </Nav>
               </Navbar.Collapse>
+              <div className='d-lg-none'>
+                <LeftSideNav></LeftSideNav>
+              </div>
             </Container>
     </Navbar>
     );
