870188acfce52513f666e12114bd975740f17291
rabby-web <rabby.webdeveloper@gmail.com>
2023-05-07 13:00:21 +0600

    react icon intall and button icon added

--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,13 +1,24 @@
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const RightSideNav = () => {
     return (
         <div>
             <ButtonGroup vertical>
-                <Button variant="outline-primary">Login with Google</Button>
-                <Button variant="outline-dark">Login with GitHub</Button>
+                <Button className='mb-2' variant="outline-primary"> <FaGoogle></FaGoogle> Login with Google</Button>
+                <Button className='mb-2' variant="outline-dark"> <FaGithub></FaGithub> Login with GitHub</Button>
             </ButtonGroup>
+            <div>
+                <h5>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'> <FaFacebook></FaFacebook> FaceBook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'> <FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'> <FaWhatsapp></FaWhatsapp> Whatsapp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'> <FaTwitch></FaTwitch> Twitch </ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
